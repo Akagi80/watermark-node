e859a0de98d1c83228c29bb1e3c878c1064cba65
Sebastian Sikora <ssikora80@gmail.com>
2021-06-17 15:54:47 +0200

    Add check files function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Jimp = require('jimp');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   const image = await Jimp.read(inputFile);
@@ -12,23 +13,26 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
 
   image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
   await image.quality(100).writeAsync(outputFile);
+  console.log('Operation add text are Success!');
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
-    const image = await Jimp.read(inputFile);
-    const watermark = await Jimp.read(watermarkFile);
-    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
   
-    image.composite(watermark, x, y, {
-      mode: Jimp.BLEND_SOURCE_OVER,
-      opacitySource: 0.5,
-    });
-    await image.quality(100).writeAsync(outputFile);
-  };
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  
+  await image.quality(100).writeAsync(outputFile);
+  console.log('Operation add image are Success!');
+};
 
   const prepareOutputFilename = (filename) => {
-    const [ name, ext ] = filename.split('.');
+    const [ name, ext ] = filename.split('.');    
     return `${name}-with-watermark.${ext}`;
   };
   
@@ -36,10 +40,10 @@ const addImageWatermarkToImage = async function(inputFile, outputFile, watermark
 
     // Ask if user is ready
     const answer = await inquirer.prompt([{
-        name: 'start',
-        message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
-        type: 'confirm'
-      }]);
+      name: 'start',
+      message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+      type: 'confirm'
+    }]);
   
     // if answer is no, just quit the app
     if(!answer.start) process.exit();
@@ -57,14 +61,19 @@ const addImageWatermarkToImage = async function(inputFile, outputFile, watermark
     }]);
 
     if(options.watermarkType === 'Text watermark') {
-        const text = await inquirer.prompt([{
-          name: 'value',
-          type: 'input',
-          message: 'Type your watermark text:',
-        }]);
-        options.watermarkText = text.value;
-        addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+      const text = await inquirer.prompt([{
+        name: 'value',
+        type: 'input',
+        message: 'Type your watermark text:',
+      }]);
+      options.watermarkText = text.value;
+
+      if (!fs.existsSync('./img/' + options.inputImage)) {
+        console.log('Something went wrong... Try again');
+        process.exit();
       }
+      addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+    }
     else {
       const image = await inquirer.prompt([{
         name: 'filename',
@@ -73,6 +82,16 @@ const addImageWatermarkToImage = async function(inputFile, outputFile, watermark
         default: 'logo.png',
       }])
       options.watermarkImage = image.filename;
+
+      if (!fs.existsSync('./img/' + options.inputImage)) {
+        console.log('Something went wrong... Try again');
+        process.exit();
+      }
+      if (!fs.existsSync('./img/' + options.watermarkImage)) {
+        console.log('Something went wrong... Try again');
+        process.exit();
+      }
+
       addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
     }  
   }
